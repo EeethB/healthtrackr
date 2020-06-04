d6df9ddc2a4944289cc81d8a74610093c58dea5d
EeethB <ethanbrockmann@gmail.com>
2020-06-04 10:12:05 -0500

    Updated functions

--- R/start.R
+++ R/start.R
@@ -19,23 +19,16 @@ init <- function(day) {
     fd_fruit = 0, fd_veg = 0
   )
 }
-init(ymd("1970-04-16"))
-
-health_hist <- init(ymd("1970-04-16"))
-
-# NA,    NA,      NA,      NA,       NA,        NA,        NA
-# ymd("1970-04-17"), 30, 45, 75, 10, 2, 3
-
-add_day <- function(hh, date) {
-  rbind(hh, init(date))
-}
-
-health_hist <- add_day(health_hist, ymd("1970-04-17"))
-health_hist
-
-health_hist %>%
-  group_by(date) %>%
-  summarise(across(.fns = sum))
+# init(ymd("1970-04-16"))
+#
+# health_hist <- init(ymd("1970-04-16"))
+#
+# add_day <- function(hh, date) {
+#   rbind(hh, init(date))
+# }
+#
+# health_hist <- add_day(health_hist, ymd("1970-04-17"))
+# health_hist
 
 add_health <- function(hh, date = today(), col, quant) {
   new_row <- init(date)
@@ -43,8 +36,7 @@ add_health <- function(hh, date = today(), col, quant) {
   hh %>%
     rbind(new_row) %>%
     group_by(date) %>%
-    summarise(across(.fns = sum)) %>%
-    print()
+    summarise(across(.fns = sum))
 }
 
 health_hist <- add_health(health_hist, col = "ex_wgt", quant = 35)
