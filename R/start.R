dabb642d0fe04b7c95a81b4564d179b311c68425
EeethB <ethanbrockmann@gmail.com>
2020-07-07 16:33:25 -0500

    Finished email parsing logic. Currently start.R can be run to kill and fill a local health tracking table.

--- R/start.R
+++ R/start.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(lubridate)
 library(purrr)
 library(stringr)
+library(readr)
 
 date_of_id <- function(i) {
   df_threads %>%
@@ -23,7 +24,7 @@ from_of_id <- function(i) {
 
 # Configure gmail auth --------------------------------------------------------
 gm_auth_configure(path = "credentials.json")
-gm_auth(cache = ".secret")
+gm_auth(email = "ejb.healthtrackr@gmail.com", cache = ".secret")
 
 # Get all threads -------------------------------------------------------------
 # threads <- gm_threads("after:2020/5/25 before:2020/6/9")
@@ -37,7 +38,8 @@ df_threads_info <- df_threads %>%
     recd = do.call(rbind, lapply(1:nrow(.), date_of_id)) %>% dmy_hms(),
     from = do.call(rbind, lapply(1:nrow(.), from_of_id))
   ) %>%
-  tidyr::unnest(cols = from)
+  tidyr::unnest(cols = from) %>%
+  filter(str_detect(from, "9207285600"))
 
 # Create a health tracker table -----------------------------------------------
 init <- function(day) {
@@ -46,60 +48,76 @@ init <- function(day) {
     ex_wgt = 0, ex_run = 0, ex_walk = 0, ex_climb = 0,
     fd_fruit = 0, fd_veg = 0,
     con_w = FALSE, con_dgt1 = FALSE, con_dgt2 = FALSE,
-    hlt_wgt = 0, hlt_rat = -1, hlt_phys = -1, hlt_ment = -1, hlt_spir = -1,
+    hlt_wgt = 0, hlt_rat = 0, hlt_phys = 0, hlt_ment = 0, hlt_spir = 0,
   )
 }
 
-add_health <- function(hh, date = today(), col, quant) {
+add_health <- function(prior, date = today(), col, quant) {
   new_row <- init(date)
   new_row[[col]] <- c(quant)
-  hh %>%
+  prior %>%
     rbind(new_row) %>%
     group_by(date) %>%
     summarise(across(.fns = sum))
 }
 
-health_hist <- init(ymd("2020-07-06"))
-
-health_hist <- add_health(health_hist, col = "ex_wgt", quant = 35)
+health_hist <- init(today())
 
 # Translate emails into health data -------------------------------------------
-words <- tibble::tribble(
-  ~word,                    ~col,
-  "run",                    "ex_run",
-  "ran",                    "ex_run",
-  "jog",                    "ex_run",
-  "lift",                   "ex_wgt",
-  "weight.*[:digit:]{1,2}", "ex_wgt",
-  "walk",                   "ex_walk",
-  "climb",                  "ex_climb",
-  "boulder",                "ex_climb",
-  "veg",                    "fd_veg",
-  "fruit",                  "fd_fruit",
-  "carlie",                 "con_w",
-  "Isla",                   "con_dgt1",
-  "Lea",                    "con_dgt2",
-  "pound",                  "hlt_wgt",
-  "weight.*[:digit:]{3,}",  "hlt_wgt"
+
+parse_body <- function(snippet, recd) {
+
+  words <- tibble::tribble(
+    ~word,                    ~col,
+    "run",                    "ex_run",
+    "ran",                    "ex_run",
+    "jog",                    "ex_run",
+    "lift",                   "ex_wgt",
+    "weight.*[:digit:]{1,2}", "ex_wgt",
+    "walk",                   "ex_walk",
+    "climb",                  "ex_climb",
+    "boulder",                "ex_climb",
+    "veg",                    "fd_veg",
+    "fruit",                  "fd_fruit",
+    "carlie",                 "con_w",
+    "isla",                   "con_dgt1",
+    "lea",                    "con_dgt2",
+    "pound",                  "hlt_wgt",
+    "weight.*[:digit:]{3,}",  "hlt_wgt",
+    "phys",                   "hlt_phys",
+    "ment",                   "hlt_ment",
+    "spir",                   "hlt_spir"
+  )
+
+  low_body <- snippet %>%
+    str_replace_all("[:punct:]", "") %>%
+    tolower()
+
+  val <- low_body %>%
+    str_extract_all("[:digit:]") %>%
+    unlist() %>%
+    paste(collapse = "") %>%
+    as.numeric()
+
+  type <- words %>%
+    mutate(match = str_detect(low_body, word)) %>%
+    filter(match) %>%
+    pluck("col", 1)
+
+  health_hist <- add_health(health_hist, col = type, quant = val, date = recd)
+
+}
+
+hlt_list <- purrr::pmap(select(df_threads_info, snippet, recd), parse_body)
+
+final <- do.call(rbind, hlt_list) %>%
+  group_by(date) %>%
+  summarise(across(.fns = sum))
+
+today()
+
+write_csv(
+  read_csv("R/Archive/health.csv"),
+  glue::glue("R/Archive/health-{today()}.csv")
 )
-
-mail_body <- "Ran for 30 min!"
-
-low_body <- mail_body %>%
-  str_replace_all("[:punct:]", "") %>%
-  tolower()
-
-val <- low_body %>%
-  str_extract_all("[:digit:]") %>%
-  unlist() %>%
-  paste(collapse = "") %>%
-  as.numeric()
-
-type <- words %>%
-  mutate(match = str_detect(low_body, word)) %>%
-  filter(match) %>%
-  pluck("col", 1)
-
-health_hist <- add_health(health_hist, col = type, quant = val)
-
-health_hist
+write_csv(final, "R/Archive/health.csv")
